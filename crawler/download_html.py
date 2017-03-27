858c5c63aefa37798a056c50a870c8db0e94e50c
unknown <>
2017-03-27 22:41:14 +0800

    finish download_html.py

--- crawler/download_html.py
+++ crawler/download_html.py
@@ -3,19 +3,26 @@ import pandas
 import re
 from bs4 import BeautifulSoup
 from os import listdir
+import pickle
 
 origin_url = "http://www.ksouhouse.com/rp.php?q=Perth&sta=wa&id="
 cur_id = 1
-req_headers = {'Host': 'www.realestate.com.au',
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36',
+req_headers = {
+'Host': 'www.ksouhouse.com',
+'Connection': 'keep-alive',
+'Cache-Control': 'max-age=0',
+'Upgrade-Insecure-Requests': '1',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36',
 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+'DNT': '1',
+'Accept-Encoding': 'gzip, deflate, sdch',
+'Cookie': 'u=0327211953589390; __utmt=1; c_sta=wa; __utma=110815135.1171698509.1490255769.1490436834.1490592034.6; __utmb=110815135.9.10.1490592034; __utmc=110815135; __utmz=110815135.1490255769.1.1.utmcsr=house.ksou.cn|utmccn=(referral)|utmcmd=referral|utmcct=/'
 }
 files = []
 
+
 def next_url():
     global cur_id, origin_url, files
-    while str(cur_id)+'.html' in files:
-        cur_id += 1
     if cur_id >260000:
         return None
     cur_url = origin_url + str(cur_id)
@@ -24,7 +31,7 @@ def next_url():
 def get_html(cur_url):
     global req_headers
     try:
-        req = urllib.request.Request(cur_url, headers=req_headers)
+        req = urllib.request.Request(cur_url)
         with urllib.request.urlopen(req) as res:
             return res.read()
     except Exception as e:
@@ -39,8 +46,16 @@ def get_html(cur_url):
 #     except Exception as e:
 #         print('No progress found.')
 def read_progress():
-    global files
+    global files,cur_id
     files = listdir('./html/')
+    file_ids = [int(i.split('.')[0]) for i in files]
+    file_ids.sort()
+    cur_id = file_ids[-1]
+    # try:
+    #     with open('invalid.dat','rb') as f:
+    #         invalid = pickle.load(f)
+    # except Exception as e:
+    #     print('No invalid file found!')
 
 def save_html(cur_html):
     global cur_id
@@ -52,6 +67,8 @@ def save_html(cur_html):
         # f.write(cur_html)
     except Exception as e:
         print(e)
+        # save_invalid()
+        
 
 def save_progress():
     global cur_id
